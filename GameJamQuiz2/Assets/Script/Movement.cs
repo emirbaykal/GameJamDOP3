dd275cb30c8510d647a15968b8e249a79c069eb6
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-12-29 18:34:42 +0300

    12

--- GameJamQuiz2/Assets/Script/Movement.cs
+++ GameJamQuiz2/Assets/Script/Movement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void PlayerMovement()
+    {
     }
 }

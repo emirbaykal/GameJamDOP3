44742c1614e90803d89e069d1509540520134c54
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-12-29 19:58:35 +0300

    Player

--- GameJamQuiz2/Assets/Script/Movement.cs
+++ GameJamQuiz2/Assets/Script/Movement.cs
@@ -1,21 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float horverSpeed;
+
+    private float hor;
+    private float ver;
+
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        PlayerMovement();
     }
 
     void PlayerMovement()
     {
+        transform.Translate(0f,0f,forwardSpeed * Time.deltaTime);
+
+        hor = Input.GetAxis("Horizontal");
+
+        ver = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hor * horverSpeed,ver * horverSpeed,0.0f);
+        rb.AddForce(movement);
+
+        rb.drag = 10;
+
+        MovementAnim();
+    }
+
+    void MovementAnim()
+    {
+        if (hor > 0)
+        {
+            gameObject.transform.DORotate(new Vector3(0,0,-25),0.2f);
+        }else if (hor == 0)
+        {
+            gameObject.transform.DORotate(new Vector3(0,0,0),0.2f);
+        }else if (hor < 0)
+        {
+            gameObject.transform.DORotate(new Vector3(0,0,25),0.2f);
+        }
+        
+        if (ver > 0)
+        {
+            gameObject.transform.DORotate(new Vector3(-25,0,0),0.2f);
+        }else if (ver == 0)
+        {
+            gameObject.transform.DORotate(new Vector3(0,0,0),0.2f);
+        }else if (ver < 0)
+        {
+            gameObject.transform.DORotate(new Vector3(25,0,0),0.2f);
+        }
     }
 }
